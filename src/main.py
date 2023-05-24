f4d08d4e161a10365165e599d3196a669217bea7
Dannoune <56166066+Dannoune@users.noreply.github.com>
2023-05-24 12:09:58 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,25 @@
+"""
+Titre du Code: Gestion de Stock
+Auteur: DANNOUNE Mohamed-Amine
+Année: 2023
+
+Ce programme permet de gérer un stock en enregistrant les produits, les ventes, les fournisseurs et les commandes.
+Il utilise plusieurs modules distincts pour chaque fonctionnalité.
+
+Modules:
+- module_stock.py: Gère le stock des produits.
+- module_ventes.py: Enregistre les ventes des produits.
+- module_fournisseurs.py: Gère les informations sur les fournisseurs.
+- module_commandes.py: Gère les commandes passées aux fournisseurs.
+
+Le programme principal instancie les modules nécessaires et interagit avec l'utilisateur pour saisir les informations
+sur les produits, les ventes, les fournisseurs et les commandes. Il met à jour le stock global en fonction des commandes
+et des ventes, et enregistre les informations dans un fichier texte.
+
+Pour exécuter le programme, exécutez la fonction main() dans ce fichier.
+
+"""
+
 import module_stock
 import module_ventes
 import module_fournisseurs
